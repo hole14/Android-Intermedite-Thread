b8448c03478cb840182399eb68f489100f253e3d
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-11 10:26:03 +0700

    Menyelesaikan loading pada MainActivity.kt

--- app/src/main/java/com/example/mythreads/MainActivity.kt
+++ app/src/main/java/com/example/mythreads/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.mythreads
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("StringFormatInvalid")
@@ -18,20 +22,28 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = android.os.Handler(Looper.getMainLooper())
+
         btnStart.setOnClickListener{
-            try{
-                //simulasi proses compress
-                for(i in 0..10){
-                    Thread.sleep(500)
-                    val percentage = i * 10
-                    if(percentage == 100){
-                        tvStatus.setText(R.string.tesk_completed)
-                    }else{
-                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+            executor.execute{
+                try{
+                    //simulasi proses compress
+                    for(i in 0..10){
+                        Thread.sleep(500)
+                        val percentage = i * 10
+
+                        handler.post{
+                            if(percentage == 100){
+                                tvStatus.setText(R.string.tesk_completed)
+                            }else{
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
                     }
+                }catch (e: InterruptedException){
+                    e.printStackTrace()
                 }
-            }catch (e: InterruptedException){
-                e.printStackTrace()
             }
         }
     }
